c9530c8b1ee5835c0e8ba94dd93ad8d39e309873
Ylyu11 <103385017+Ylyu11@users.noreply.github.com>
2022-04-17 19:21:03 -0700

    Session 1 prompt for Coding Journal 1

---
+++ Code1.py
@@ -0,0 +1,23 @@
+#Problem 1
+print("My name is Yi Lyu. My preferred pronouns are she/her/hers. My favorite movie is the Shining. My favorite food is ice cream. ")
+
+#Problem 2
+a = [30.56, 21.32]
+total = sum(a)
+print(total)
+
+b = 10 - 5
+print(b)
+
+c = 7.89 * 10
+print(c)
+
+#Problem 3
+def f_x(x):
+    y = x**3 + 8
+    if y > 27:
+        print("YAY!")
+f_x(9)
+
+#Problem 4
+
