8b042b2da178f69c663a0747b1e4e5eb356c7b9f
Ylyu11 <103385017+Ylyu11@users.noreply.github.com>
2022-04-26 16:56:35 -0700

    Update cj1-4.py

--- cj1-4.py
+++ cj1-4.py
@@ -5,7 +5,7 @@ class Animal:
     self.number_of_eyes = number_of_eyes
   def myfunc(self):
     print("The name of the animal is:" + self.name)
-    print("The length of the leg is:", self.length_of_the_legs)
+    print("The length of the leg is:", self.length_of_the_legs, "cm")
     print("The number of eye is:", self.number_of_eyes)
 cat = Animal("cat", 43, 2)
 cat.myfunc()
